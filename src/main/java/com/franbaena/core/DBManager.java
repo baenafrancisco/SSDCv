03b7ca724846b8d173de2d35da24d831fb0ed61d
Francisco Baena <soy@franbaena.com>
2015-04-26 18:23:34 +0100

    Updated interface schema, implemented SQLite's Select method.

--- src/main/java/com/franbaena/core/DBManager.java
+++ src/main/java/com/franbaena/core/DBManager.java
@@ -1,14 +1,27 @@
 package com.franbaena.core;
+import java.sql.*;
+import java.util.*;
+
 
 /**
  * Database Manager Interface
  * @author Francisco Baena
+ *
+ * The DBManager is a helper class that implements the DBManager interface
+ * its aim is offer a simpler query interface for standard CRUD SQLite Connections
+ * and be the base of a pseudo-ORM core 
+ *
+ * This code has been developed for the Unit Software Systems Development 
+ * @ Southampton Solent University.
+ *
  */
 
 public interface DBManager{
-   public Object[] select(String database_table, String[] columns, String where);
-   public boolean insert(String database_table, Object[] values);
-   public boolean update(String database_table, Object object);
-   public boolean delete(String database_table, int id);
-   public boolean exists(String database_table, int id);
+	public List<Map<String, Object>> select(String database_table);
+	public List<Map<String, Object>> select(String database_table, String[] columns);
+	public List<Map<String, Object>> select(String database_table, String[] columns, String where);
+	public boolean insert(String database_table, Map<String, String> values);
+	public boolean update(String database_table, Map<String, String> values);
+	public boolean delete(String database_table, int id);
+	public boolean exists(String database_table, int id);
 }
--- src/main/java/com/franbaena/core/SQLiteManager.java
+++ src/main/java/com/franbaena/core/SQLiteManager.java
@@ -1,16 +1,23 @@
 package com.franbaena.core;
 import java.sql.*;
-import java.util.Arrays;
-import java.lang.reflect.Field;
+import java.util.*;
+import java.lang.*;
 
 /**
  * SQLite Manager
  * @author Francisco Baena
  *
- * The SQLite (DB) manager implements the DBManager interface.
+ * The SQLiteManager is a helper class that implements the DBManager interface
+ * its aim is offer a simpler query interface for standard CRUD SQLite Connections
+ * using Java Standard libraryes, in order to be the base of a pseudo-ORM core.
+ *
+ * This code has been developed for the Unit Software Systems Development 
+ * @ Southampton Solent University. It's main goal is denmostrating knowledge
+ * in OOP, GRASP Patters… AND MIGHT NOT BEEN OPTIMUM.
  *
  */
-public class SQLiteManager implements DBManager{
+public class SQLiteManager 
+	implements DBManager{
 
 	private Connection c;
    	public SQLiteManager(){
@@ -26,25 +33,78 @@ public class SQLiteManager implements DBManager{
    		}
    	}
 
-   	public Object[] select(String database_table, String[] columns, String where){
+   	public List<Map<String, Object>> select(String database_table, String[] columns, String where){
    		/** 
 		* Selects values from a database table
 		*
-		* 
 		* @param database_table  name of the database table
 		* @param columns array of column names to fetch
+		* @param where string
+		*
+		* @return returns a List<Map<String,Object>>, representing a list of results
 		* 
-		* @return Object[] 
    		*/
-   		return new Object[0];
+
+		List<Map<String, Object>> response = new ArrayList<Map<String, Object>>();
+   		String sql;
+   		Statement stmt;
+   		ResultSetMetaData rsmd;
+   		ResultSet rs = null;
+   		int numberOfColumns;
+
+   		try{
+   			// 
+   			if (columns!=null){
+	   			sql = "SELECT " + String.join(", ", columns) + " FROM " + database_table;
+	   			if (where!=null){
+	   				sql += " WHERE " + where;
+	   			}
+	   			sql += ";";
+	   		} else {
+	   			sql = "SELECT * FROM " + database_table + ";";
+	   		}
+
+	   		stmt = c.createStatement();
+	   		rs = stmt.executeQuery( sql );
+	   		rsmd = rs.getMetaData();
+	   		numberOfColumns = rsmd.getColumnCount();
+	   		while(rs.next()) {
+	   			Map<String, Object> result = new TreeMap<String, Object>();
+	   			// Result set columns start at 1!
+	   			for (int i=1; i<=numberOfColumns; i++){
+	   				result.put(rsmd.getColumnName(i), rs.getObject(i));
+	   			}
+	   			response.add(result);
+	    	}
+
+   		} catch (Exception e){
+   			System.out.println("An exception ocurred trying to select from" + database_table);
+   			e.printStackTrace(System.out);
+   			System.exit(1);
+   		}
+   		return response;
    	}
 
-   	public boolean insert(String database_table, Object[] values){
+   	public List<Map<String, Object>> select(String database_table){
+   		/** 
+		* Selects all values from the database table
+   		*/
+   		return select(database_table, null, null);
+   	}
+
+   	public  List<Map<String, Object>> select(String database_table, String[] columns){
+   		/** 
+		* Selects certain columns from the database table
+   		*/
+   		return select(database_table, columns, null);
+   	}
+
+   	public boolean insert(String database_table, Map<String, String> values){
    		// TODO
    		return false;
    	}
 
-   	public boolean update(String database_table, Object object){
+   	public boolean update(String database_table, Map<String, String> values){
    		// TODO
    		return false;
    	}
