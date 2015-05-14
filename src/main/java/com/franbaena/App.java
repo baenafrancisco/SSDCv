0b31ae80a7f9c5aea8c718bf390038fb795d324c
Francisco Baena <soy@franbaena.com>
2015-05-14 08:26:22 +0100

    Checkout App.java

--- src/main/java/com/franbaena/App.java
+++ src/main/java/com/franbaena/App.java
@@ -1,12 +1,13 @@
 package com.franbaena;
-import com.franbaena.core.*;
 
 /**
  * Hello world!
  *
  */
-public class App {
-    public static void main( String[] args ){
-    	DBManager db = new SQLiteManager();
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
